196e37a4ce0a6c298359f576df75cb9a616c9b19
Li Jingyuan <nearfar1jy@gmail.com>
2023-12-18 14:18:51 +0800

    添加通信数据帧框架

---
+++ Core/Src/serialFrame.c
@@ -0,0 +1,18 @@
+/**
+ * @file serialFrame.c
+ * @brief 定义通信的数据帧，做接收数据的解析
+ *
+ * 本文件实现通信相关的函数，不同设备间应保有相同的收发协议
+ *
+ * @author Jingyuan Li (nearfar1jy@email.com)
+ * @version 1.0
+ * @date 2023-12-14
+ *
+ * @copyright Copyright (c) 2023 Jingyuan Li
+ *
+ * @usage: 
+ *
+ */
+
+#include"serialFrame.h"
+
